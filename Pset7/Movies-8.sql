aa3d8942f1e39b6fff402571da66552469663fdc
Mateusz <66701870+Moxiis@users.noreply.github.com>
2021-03-15 09:25:29 +0100

    Create Movies-8.sql

---
+++ Pset7/Movies-8.sql
@@ -0,0 +1,1 @@
+SELECT name FROM people INNER JOIN stars ON people.id = stars.person_id, movies ON stars.movie_id = movies.id WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND movies.title = "Toy Story"
