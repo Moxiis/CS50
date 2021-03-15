9369f08949cf21595d81ab73ac1f25505957b2ec
Mateusz <66701870+Moxiis@users.noreply.github.com>
2021-03-15 09:26:08 +0100

    Update Movies-9.sql

--- Pset7/Movies-9.sql
+++ Pset7/Movies-9.sql
@@ -1,4 +1,1 @@
-SELECT DISTINCT name FROM people
-INNER JOIN stars ON people.id = stars.person_id, movies ON stars.movie_id = movies.id WHERE movies.id IN
-(SELECT movies.id FROM movies INNER JOIN stars ON movies.id  = stars.movie_id, people ON stars.person_id = people.id WHERE people.name = "Kevin Bacon" AND people.birth = 1958)
-AND people.name != "Kevin Bacon"
+SELECT DISTINCT people.name FROM people INNER JOIN stars ON people.id = stars.person_id, movies ON stars.movie_id = movies.id WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND movies.year = 2004 ORDER BY people.birth
